0b8844094f38dde9271605946787b9664a6e90f3
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-08-14 11:03:17 -0500

    Word-Guess-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,18 +10,21 @@ let wrongLetter = [];
 function startGame(){
     currentWord = words[Math.floor(Math.random() * words.length)];
 
-//Test
-    console.log(currentWord);
+//test
+console.log(currentWord)
 
  //Creates underscores depending on length of the currentWord; push underscores to dashes array 
     for ( let i = 0; i < currentWord.length; i++ ) {
         dashes.push(' _ ');
     }
     return dashes;
-    
+  
     let formattedDashes =  dashes.join(' ');
-    document.getElementById('dashContainer').innerHTML = formattedDashes;  
+    document.getElementByClassName('dashContainer').innerHTML = formattedDashes;  
 }
+
+//test
+console.log(startGame());
 
 //HTML document waits for key to be pressed; key is assiged Unicode value
     document.addEventListener('keypress', (event) => {
@@ -29,14 +32,16 @@ function startGame(){
 
 //Convert a Unicode value into a character
         let characters = String.fromCharCode(codes);
-//Test        
-        console.log(characters)
 
 //Search currentWord for the users correct guess; push character to rightLetter array
         if(currentWord.indexOf(characters) > -1)
         rightLetter.push(characters);
-//Test
-        console.log(rightLetter);
+
+//Replace dashes with correctly choosen characters
+        dashes[currentWord.indexOf(characters)] = characters;
+        console.log(dashes);
+
+        
 
 //Search currentWord for the users incorrect guess; push character to wrongLetter array
         if(currentWord.indexOf(characters) < 0)
@@ -46,10 +51,10 @@ function startGame(){
     })
 
 //Start game function
-startGame()
+//startGame()
 
 
 //Test Example
-document.getElementById("test").addEventListener("click",function(){
-    alert("JavaScript is Working")
+document.getElementById("start").addEventListener("click",function(){
+    alert("Good Luck!")
 })
