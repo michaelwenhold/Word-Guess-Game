0f0b5f64698ca07fff2c1168909fa35ee1f818fb
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-08-13 21:34:40 -0500

    Word-Guess-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,37 @@
-let words = ["cat", "dog", "horse", "elephant"];
-let currentWord;
-var dashes = [];
+//Array of available words
+const words = ["Patriots", "Bills", "Dolphins", "Jets", "Chiefs", "Chargers", "Raiders", "Broncos", "Steelers", "Ravens", "Bengals", "Browns", "Jaguars", "Titans", "Colts", "Texans", "Eagles", "Cowboys", "Redskins", "Giants", "Rams", "Seahawks", "Cardinals", "Niners", "Vikings", "Lions", "Packers", "Bears", "Saints", "Panthers", "Falcons", "Buccaneers"];
 
-//start game
+let currentWord;
+let dashes = [];
+
+//Computer chooses a random word
 function startGame(){
-    currentWord = words[Math.floor(Math.random() * 4)];
+    currentWord = words[Math.floor(Math.random() * 32)];
     console.log(currentWord);
 
-    for ( var i = 0; i < currentWord.length; i++ ) {
+ //Creates dashes depending on length of the word chosen  
+    for ( let i = 0; i < currentWord.length; i++ ) {
         dashes.push(' _ ');
     }
-
-    var formattedDashes =  dashes.join(' ');
+    return dashes;
     
-    document.getElementById('dashContainer').innerHTML = formattedDashes;
-    
+    let formattedDashes =  dashes.join(' ');
+    document.getElementById('dashContainer').innerHTML = formattedDashes;   
 }
 
+//Guessed letter gets Unicode value
+    document.addEventListener('keypress', (event) => {
+        let codes = event.keyCode;
+//Convert a Unicode number into a character
+        let characters = String.fromCharCode(codes);
+        console.log(characters)
+    })
 
+//Start game function
 startGame()
 
 
-
+//Test Example
 document.getElementById("test").addEventListener("click",function(){
     alert("JavaScript is Working")
 })
